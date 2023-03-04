35d9279f6ec65c4a36cf861b40db5b905dad6265
JPannn <js768js@gmail.com>
2023-03-04 04:04:09 -0600

    Really Finished HTML

---
+++ script.js
@@ -0,0 +1,10 @@
+// Create a Quiz App using JavaScript, HTML and CSS. The app should have the following features:
+// The user will be able to select a diffuculty level
+// (easy, medium, hard) based on the difficulty level the user will be presented with a different number of questions.
+// then, display a question based on an image and 4 possible answers.
+// The user will select an answer and then move onto the next question until all questions have been answered.
+// The user will be able to see their score at the end of the quiz.
+
+// Create a function that will display the questions and answers
+// Create a function that will check the answer
+// Create a function that will display the score
